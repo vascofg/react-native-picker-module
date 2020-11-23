5b12b8109e2f4b0b790ab712cbba803fa01f3922
Vasco Gonçalves <vasg@saxobank.com>
2020-11-23 16:09:14 +0100

    more fixes

--- android/src/main/java/com/taluttasgiran/pickermodule/ReactNativePickerModuleModule.java
+++ android/src/main/java/com/taluttasgiran/pickermodule/ReactNativePickerModuleModule.java
@@ -32,7 +32,7 @@ public class ReactNativePickerModuleModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void hide() {
-        if (rnSpinner == null) {
+        if (rnSpinner != null) {
             rnSpinner.hide();
         }
     }
